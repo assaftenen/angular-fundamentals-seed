a6a3f54fee8339f0e4d06cc38cb97b88d6df6c08
Assaf <assaf.tenen@gmail.com>
2017-01-02 15:21:41 +0200

    Tow ways data binding ngModelChange/ [(ngModel)]

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,36 @@
+import { NgModel } from '@angular/forms/src/directives';
+import { selector } from 'rxjs/operator/publish';
 import { Component } from '@angular/core';
 
+
 @Component({
-  selector: 'app-root',
-  styleUrls: ['app.component.scss'],
-  template: `
-    <div class="app">
-      Hello!
-    </div>
-  `
+selector: 'app-root',
+styleUrls:['app.component.scss'],
+template: `
+
+<div class="app">
+<button>
+Change Name
+</button>
+<input 
+type="text"
+[ngModel]="name"
+(ngModelChange)="handleChange($event)"/>
+
+<input 
+type="text"
+[(ngModel)]="name"/>
+
+{{name}}
+<div>`
 })
-export class AppComponent {
+
+export class AppComponent{
+name:string = 'Assaf'
+
+handleChange(value: string) {
+this.name = value;
+  console.log(value);
+}
 
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,19 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-
 import { AppComponent } from './app.component';
+FormsModule
 
 @NgModule({
-  imports: [
+  declarations:[
+    AppComponent
+  ],
+  imports:[
     BrowserModule,
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
-  bootstrap: [
-    AppComponent
-  ],
-  declarations: [
-    AppComponent
-  ]
+  bootstrap:[AppComponent]
 })
 export class AppModule {}
