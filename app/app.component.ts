56b7ef27f2a16af51aa137c1e62034e13b13b305
Assaf <assaf.tenen@gmail.com>
2017-01-02 15:32:06 +0200

    # ref

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,33 +4,28 @@ import { Component } from '@angular/core';
 
 
 @Component({
-selector: 'app-root',
-styleUrls:['app.component.scss'],
-template: `
+  selector: 'app-root',
+  styleUrls: ['app.component.scss'],
+  template: `
 
 <div class="app">
-<button>
-Change Name
-</button>
-<input 
-type="text"
-[ngModel]="name"
-(ngModelChange)="handleChange($event)"/>
-
-<input 
-type="text"
-[(ngModel)]="name"/>
-
-{{name}}
+  <button (click)="handleClick(userName.value)">
+        Get Value
+  </button>
+  <input type="text" #userName/>
+      {{name}}
 <div>`
 })
 
-export class AppComponent{
-name:string = 'Assaf'
+export class AppComponent {
+  name: string = 'Assaf'
 
-handleChange(value: string) {
-this.name = value;
-  console.log(value);
-}
+  handleChange(value: string) {
+    this.name = value;
+    console.log(value);
+  }
+  handleClick(value: string) {
+    console.log(value);
+  }
 
 }
