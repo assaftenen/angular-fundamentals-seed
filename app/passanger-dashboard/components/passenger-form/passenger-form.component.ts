fd5ea2053ece08257857106dda1a842cc7cc805b
Assaf <assaf.tenen@gmail.com>
2017-01-14 19:38:58 +0200

    ng form OWB input , form reference, name

--- app/passanger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passanger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,14 +1,32 @@
+import { PassengerDetailComponent } from './../passenger-detail/passenger-detail.component';
+import { format } from 'url';
+import { format } from 'util';
+import { type } from 'os';
+import { NgForm, NgModel } from '@angular/forms/src/directives';
 import { Passenger } from '../../modules/passenger.interface/passenger.interface';
 import { Component, Input } from '@angular/core';
 
 @Component({
 selector: 'passenger-form',
 styleUrls:['passenger-form.component.scss'],
-template: `<form>
+template: `<form #form ="ngForm" novalidate>
+                {{detail | json}}
             <div>
-            Form!
-            {{detail | json}}
+            Passenger name:
+            <input 
+            type="text"
+            name="id"
+            [ngModel]="detail?.id">
             </div>
+
+            <div>
+            Passenger ID:
+            <input 
+            type="number"
+            name="fullname"
+            [ngModel]="detail?.name">
+            </div>
+            {{form.value | json}}
         </form>`
 
 })
