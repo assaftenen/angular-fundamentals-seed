159c1016e55ac91bcd704852b59e47ce993a5e32
Assaf <assaf.tenen@gmail.com>
2017-01-15 11:03:03 +0200

    simple route , outlet

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,21 +1,28 @@
+import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { PassangerDashBoardModule } from './passanger-dashboard/passenger-dashboard.module';
-
 import { AppComponent } from './app.component';
-FormsModule
+import { HomeComponent } from './home.component';
+
+const routes:Routes = [
+{path:'', component: HomeComponent, pathMatch:'full'}
+  
+]
 
 @NgModule({
   declarations:[
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports:[
     //angular Module
     BrowserModule,
     CommonModule,
     FormsModule,
+    RouterModule.forRoot(routes),
 
    //custom Module
     PassangerDashBoardModule
---
+++ app/home.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector:'app-home',
+    template:`
+    <div>
+    Airline passenger app!
+    </div>`
+}
+)
+
+
+export /**
+ * HomeComponent
+ */
+class HomeComponent {
+
+}
