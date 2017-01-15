5bc29161515593d79e601ae3478edc3ef2932393
Assaf <assaf.tenen@gmail.com>
2017-01-15 11:38:04 +0200

    rout link home 404

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,3 +1,5 @@
+import { HomeComponent } from './home.component';
+import { RouterLink } from '@angular/router';
 import { NgIf } from '@angular/common/src/directives/ng_if';
 import { NgModel } from '@angular/forms/src/directives';
 import { selector } from 'rxjs/operator/publish';
@@ -9,6 +11,13 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
 <div class="app">
+
+<a routerLink="/">
+  Home
+</a>
+<a routerLink="/oops">
+  404
+</a>
   <router-outlet></router-outlet>
 </div>`
 })
--- app/not-found.component.ts
+++ app/not-found.component.ts
@@ -1,11 +1,15 @@
+import { Router, RouterLink } from '@angular/router';
 import { Component } from '@angular/core';
+
+
 
 
 @Component({
     selector:'not-found',
     template:`
     <div>
-    404
+    Not Found
+   <a routerLink="/">Go Home</a>?
     </div>`
 }
 )
